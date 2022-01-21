0dc8b90d08940fa66c07c01b586ebdff57d83a81
unknown <hindhujhasridhar@gmail.com>
2022-01-21 11:18:31 +0530

    ADDING 3 INTEGERS TO ZERO

---
+++ Array/SUM.js
@@ -0,0 +1,13 @@
+//sum of 3 integers adds to zero
+const prompt=require("prompt-sync")();
+function sum(i)
+{ 
+count+=i
+console.log("while adding integers :",count);
+}
+
+let count=0,arr=[5,4,2],i;
+sum(arr[0])
+sum(arr[1])
+sum(arr[2])
+console.log("SUM IS: ",count);
